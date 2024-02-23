9fa651211e7cdaa542c97362ce04ff88899039f8
Alan Tanori <suza.ortiz4@gmail.com>
2024-02-23 12:39:33 -0700

    Agregue notificaciones de confirmación para eliminar/modificar.

--- php/conexionbd.php
+++ php/conexionbd.php
@@ -1,12 +1,12 @@
 <?php
-    $host = "localhost";
-    $user = "sistemaweb3";
-    $pass = "sistemaweb3";
-    $db = "sistemaweb3";
+	$host = "localhost";
+	$user = "root";
+	$pass = "";
+	$db = "sistemaweb";
 
-    $conn = new mysqli($host,$user,$pass,$db);
+	$conn = new mysqli($host,$user,$pass,$db);
 
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
+	if (!$conn) {
+		echo "Error en la conexión";
+	}
 ?>
